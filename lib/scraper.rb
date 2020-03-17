019d714c8497cecdf2ba1647c442a7c533b6b971
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-03-17 12:00:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,3 +5,7 @@ html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 
 courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+
+courses.each do |course|
+  puts course.text.strip
+end
